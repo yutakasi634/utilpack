efde8663c4776c052d3c74cb538caa1685785219
yutaka <p7dea87c7e4n7j@gmail.com>
2017-08-22 19:32:22 +0900

    fix row and column confuse and add columnbegin and end test.

--- test_for_2d-matrix.cpp
+++ test_for_2d-matrix.cpp
@@ -12,20 +12,26 @@ int main()
     cout  << "generate array_matrix<int, 0, 0>" << endl;    
     array_matrix<int, 0, 0> empty_matrix;
     cout << "judge empty = " << boolalpha << empty_matrix.empty() << endl;
-    const size_t culum_Num{3}, row_Num{4};
+    const size_t column_Num{3}, row_Num{4};
     cout  << "generate array_matrix<int, "
-	  << culum_Num << ", " << row_Num << ">" << endl;
-    array_matrix<int, culum_Num, row_Num> test_matrix;
+	  << column_Num << ", " << row_Num << ">" << endl;
+    array_matrix<int, row_Num, column_Num> test_matrix;
     cout << "judge empty = " << boolalpha << test_matrix.empty() << endl;
     cout << "fill with 1." << endl;
     test_matrix.fill(1);
     cout << "elem num = " << test_matrix.size() << endl;
     cout << "row num = " << test_matrix.xsize() << endl;
-    cout << "culum num = " << test_matrix.ysize() << endl;
+    cout << "column num = " << test_matrix.ysize() << endl;
 
+    cout << "access and fill with rowcolumn num" << endl;
+    for(size_t i = 0; i < row_Num; ++i){
+	for(size_t j = 0; j < column_Num; ++j)
+	    test_matrix.at(i,j) = i * 10 + j;
+    }
+    
     cout << "access and output." << endl;
-    for(size_t i = 0; i < culum_Num; ++i){
-	for(size_t j = 0; j < row_Num; ++j)
+    for(size_t i = 0; i < row_Num; ++i){
+	for(size_t j = 0; j < column_Num; ++j)
 	    cout << test_matrix.at(i,j) << " ";
 	cout << endl;
     }
@@ -35,18 +41,20 @@ int main()
 	cout << elem << " ";
     cout << endl;
 
-    cout << "fill with rowculum num" << endl;
-    for(size_t i = 0; i < culum_Num; ++i){
-	for(size_t j = 0; j < row_Num; ++j)
-	    test_matrix.at(i,j) = i * 10 + j;
-    }
-
     cout << "test rowbegin() and rowend()." << endl;
-    for(array_matrix<int, row_Num, culum_Num>::iterator itr =
-	    test_matrix.rowbegin(row_Num / 2);
-	itr != (test_matrix.rowend(row_Num / 2)); ++itr)
+    for(array_matrix<int, row_Num, column_Num>::row_iterator itr =
+	    test_matrix.rowbegin(2);
+	itr != (test_matrix.rowend(2)); ++itr)
 	cout << *itr << " ";
     cout << endl;
+
+    cout << "test columnbegin() and columnend() for const matrix." << endl;
+    for(array_matrix<int, row_Num, column_Num>::column_iterator itr =
+	    test_matrix.columnbegin(2);
+	itr != (test_matrix.columnend(2)); ++itr)
+	cout << *itr << " ";
+    cout << endl;
+    
     return 0;
 }
 
